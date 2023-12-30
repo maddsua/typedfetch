fde9573279b3ba49b607601f52739a59ef9d0a03
maddsua <maddsuadev@proton.me>
2023-12-30 08:09:34 +0200

    Update router.ts

--- test/router.ts
+++ test/router.ts
@@ -4,7 +4,8 @@ import { ServerRouter } from "../v2/server.ts";
 const routerMethods = {
 	test: method((props: { test: number }) => ({ result: 22 }), {
 		type: 'query'
-	})
+	}),
+	check: method(() => ({ live: true }),)
 };
 
 const handler = (props: { test: number }) => ({ result: 22 });
@@ -19,7 +20,7 @@ const router = new ServerRouter(routerMethods, {
 
 type RouterType = typeof router;
 
-const testRequest = new Request('http://test.com/api/test', {
+const testRequest = new Request('http://test.com/api/check/', {
 	headers: {
 
 	}
