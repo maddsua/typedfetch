f58ee5e5cf51ff85953d2a88d5be084b0c9bffad
maddsua <maddsuadev@proton.me>
2023-12-30 07:23:52 +0200

    Update server.ts

--- v2/server.ts
+++ v2/server.ts
@@ -6,15 +6,41 @@ interface RouterOptions {
 	endpoint?: string | URL;
 };
 
+const getNormalizedEndpoint = (endpoint: string | URL) => {
+	const pathname = typeof endpoint === 'string' ? (/^http(s)?\:\/\//.test(endpoint) ? new URL(endpoint).pathname : (endpoint.startsWith('/') ? endpoint : '/' + endpoint)) : endpoint.pathname;
+	return pathname;
+};
+
 export class ServerRouter<T extends BasicTypedRouter> {
 
 	router: T;
 	endpointPath: string = '/';
 
-	constructor (router: T, endpointURL: URL) {
+	constructor (router: T, opts?: RouterOptions) {
+
 		this.router = router;
-		this.endpointPath = endpointURL.pathname;
+
+		let pathname: string;
+		switch (typeof opts?.endpoint) {
+
+			case 'string': {
+
+				if (/^http(s)?\:\/\//.test(opts.endpoint)) {
+					pathname = new URL(opts.endpoint).pathname;
+				} else {
+					pathname = (opts.endpoint.startsWith('/') ? opts.endpoint : '/' + opts.endpoint);
+				}
+
+			} break;
+
+			case 'object': {
+				pathname = opts.endpoint.pathname;
+			} break;
+		
+			default: pathname = '/';
+		}
+		
+		this.endpointPath = pathname.endsWith('/') ? pathname : pathname + '/';
 	};
 
 };
-
