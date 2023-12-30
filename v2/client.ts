4093ff03e85a3dd4fedff2822ea35291fe3495d8
maddsua <maddsuadev@proton.me>
2023-12-30 09:46:38 +0200

    Update client.ts

--- v2/client.ts
+++ v2/client.ts
@@ -1,10 +1,10 @@
-import type { RouterType, TypedRouterMethodOptions } from "./router.ts";
+import type { RouterType } from "./router.ts";
 
 
 export class ClientRouter<T extends RouterType<any, any, any>> {
 
 	endpoint: string = '/';
-	query: { [K in keyof T]: T[K]['opts']['type'] extends 'mutation' ? T[K]['opts']['type'] : T[K]['handler'] };
+	query: { [K in keyof T]: T[K]['opts']['type'] extends 'mutation' ? never : T[K]['handler'] };
 	mutate: { [K in keyof T]: T[K]['opts']['type'] extends 'mutation' ? T[K]['handler'] : never };
 
 	constructor (endpoint: string) {
